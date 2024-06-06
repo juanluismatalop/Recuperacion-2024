a04558a92db22030500f1444d7e5cabc1819fe76
julu <juanluismatalop@gmail.com>
2024-06-06 10:15:07 +0100

    commit 6/6/24 primer fichero leido

---
+++ src/Ejercicio2/Main.java
@@ -0,0 +1,18 @@
+package Ejercicio2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> lineas;
+        try {
+            lineas = Files.readAllLines(Paths.get("Ficheros/ejercicio2.csv"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(lineas);
+    }
+}

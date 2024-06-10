9e73cd70b886d55efc87138af2992d5a585a51f5
julu <juanluismatalop@gmail.com>
2024-06-10 12:23:13 +0100

    commit 10/06/2024 clientes

--- src/Ejercicio3/Clientes.java
+++ src/Ejercicio3/Clientes.java
@@ -1,5 +1,9 @@
 package Ejercicio3;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,16 +28,49 @@ public class Clientes {
         return listaPersonas.remove(persona.email.matches(email));
     }
 
-    public boolean actualizarPersona() {
+    public boolean actualizarPersona(Persona persona, String nuevoEmail, String nuevoFirstName, String nuevoLastName, Genero nuevoGender) {
         return false;
     }
 
-    public boolean buscarPersonaEmail() {
-        return false;
+    public boolean buscarPersonaEmail(String email) {
+        Persona persona = null;
+        if (persona.email.matches(email)) {
+            System.out.println(persona);
+            return true;
+        }
+        else
+            return false;
     }
 
-    public int numeroPersonaGenero() {
-        return 0;
+    public int numeroPersonaGenero(String gender, List<Persona> personas, String ruta) throws IOException {
+        List<Persona> listaGenero = new ArrayList<>();
+        Path path = Paths.get(ruta);
+        List<String> lineas;
+        lineas = Files.readAllLines(path);
+        for (int i = 1; i< lineas.size(); i++){
+            String[] tokens = lineas.get(i).split(",");
+            int id = Integer.parseInt(tokens[0]);
+            String firstName = tokens[1];
+            String lastName = tokens[2];
+            String email = tokens[3];
+            Genero genero = Genero.valueOf(gender);
+            Persona persona = new Persona(id, firstName, lastName, email, genero);
+            listaGenero.add(persona);
+        }
+        return listaGenero.size();
+    }
+    private static Genero obtenerEnumGender(String genero){
+        switch (genero){
+            case "Male" -> {return Genero.MALE;}
+            case "Female" -> {return Genero.FEMALE;}
+            case "Genderfluid" -> {return Genero.GENDERFLUID;}
+            case "Genderqueer" -> {return Genero.GENDERQUEER;}
+            case "Non_binary" -> {return Genero.NON_BINARY;}
+            case "Bigender" -> {return Genero.BIGENDER;}
+            case "Polygender" -> {return Genero.POLYGENDER;}
+            case "Agender" -> {return Genero.AGENDER;}
+        }
+        return null;
     }
 
     public List<Persona> personasPorDominioEmail() {
